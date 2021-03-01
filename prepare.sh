cda9701448f1afe22ef4169291fad0388c61e8db
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-03-01 13:11:34 -0800

    updates

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 MY_DIR="$(dirname $MY_PATH)"
-cd $MY_DIR
+cd ${MY_DIR}
+
+cd src
 
 rm -rf node_modules/
-rm -rf dist/
 
 npm install
-npm install --only=dev
-npm update the-lodash the-promise
-tsc
+npm update the-lodash the-promise  @kubevious/helpers @kubevious/ui-framework @kubevious/ui-middleware
+
+${MY_DIR}/build.sh
