e91c994b893880880928e12c729e45c578dc3966
Semyon Pukhov <semyonpuchof@gmail.com>
2021-03-02 14:55:54 +0300

    Remove diagram with dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import CopyClipboardComponent from './CopyClipboard/index'
-import DiagramComponent from './Diagram/index'
 import ErrorBoxComponent from './ErrorBox/index'
 
 export const ErrorBox = ErrorBoxComponent;
 export const CopyClipboard = CopyClipboardComponent;
-export const Diagram = DiagramComponent;
