ebb0ad2e6616e5b9982b10aaa3774f0c64e9f900
Semyon Pukhov <semyonpuchof@gmail.com>
2021-03-17 11:35:20 +0300

    Update export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 export { DnComponent } from './DnComponent/index'
+
 export { DnPath } from './DnPath/index'
-export { ErrorBox } from './ErrorBox/index'
+
 export { CopyClipboard } from './CopyClipboard/index'
+
 export { DnShortcutComponent } from './DnShortcutComponent/index'
+
 export { Popup } from './Popup/index'
+
 export { GoldenLayout } from './GoldenLayout/index'
 export { GoldenLayoutWindowInfo, GoldenLayoutLocation, GoldenWindowCloseHandler } from './GoldenLayout/types'
+
+export { ErrorBox } from './ErrorBox/index'
+export { Error } from './ErrorBox/types'
