1f7f6a9abadd8d098728b7dc251bebada06522cb
Semyon Pukhov <semyonpuchof@gmail.com>
2021-05-04 14:36:00 +0300

    Update Tab component styles

--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -8,14 +8,14 @@ export default {
 };
 
 export const Default: Story = () => (
-    <div style={{ background: '#1e1e1e', color: 'white' }}>
+    <div style={{ background: '#212122', color: 'white', padding: '1rem' }}>
         <Tabs>
-            <Tab key="1" label="First">
-                First
+            <Tab key="1" label="Edit Rule">
+                Edit rule...
             </Tab>
 
-            <Tab key="2" label="Second">
-                Second
+            <Tab key="2" label="Affected Objects (16)">
+                Affected Objects (16)
             </Tab>
         </Tabs>
     </div>
