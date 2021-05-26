c94e350f11cba0737cbf2e2289dbe403f73b935d
Semyon Pukhov <semyonpuchof@gmail.com>
2021-05-26 14:10:39 +0300

    Add collapsed menu items hover

--- src/SideMenu/index.tsx
+++ src/SideMenu/index.tsx
@@ -1,6 +1,6 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import cx from 'classnames';
 
@@ -43,85 +43,116 @@ export interface SideMenuProps {
     isCollapsed: boolean;
 }
 
-export const SideMenu: FC<SideMenuProps> = ({ sections, footer, isCollapsed }) => (
-    <aside className={cx(styles.container, { [styles.collapsed]: isCollapsed })}>
-        <div style={{ height: '100%' }} className="d-flex flex-column justify-content-between">
-            <div>
-                <div className={styles.header}>
-                    {!isCollapsed && <span className={styles.beta}>BETA</span>}
-                    {isCollapsed ? <img src="/img/logoSmall.svg" /> : <img src="/img/logoBig.svg" />}
-                </div>
+export const SideMenu: FC<SideMenuProps> = ({ sections, footer, isCollapsed }) => {
+    const [showItem, setShowItem] = useState<string | null>(null);
 
-                <div className={cx(styles.bar, { [styles.barLoading]: true })}></div>
+    const handleMouseEnter = (item: SideMenuItem) => {
+        if (!isCollapsed) {
+            return;
+        }
 
-                <div style={{ marginTop: isCollapsed ? '30px' : 0 }}>
-                    {sections.map((section, index) => (
-                        <div key={index}>
-                            {!isCollapsed && <div className={styles.sectionLabel}>{section.name}</div>}
+        setShowItem(item.key);
+    };
 
-                            <div>
-                                {section.items.map((item) =>
-                                    item.url ? (
-                                        <NavLink
-                                            className={cx(styles.itemBlock)}
-                                            activeClassName={styles.selectedItem}
-                                            to={item.url}
-                                            key={item.key}
-                                        >
-                                            {item.icon ? (
-                                                <img src={`/img/menu/${item.icon}`} />
-                                            ) : (
-                                                <FontAwesomeIcon
-                                                    icon={item.faIcon!}
-                                                    style={{
-                                                        width: '24px',
-                                                        height: '24px',
-                                                    }}
-                                                />
-                                            )}
-                                            {!isCollapsed && (
-                                                <span className={cx('ms-4', styles.itemLink)}>{item.label}</span>
-                                            )}
-                                        </NavLink>
-                                    ) : (
-                                        <div className={cx(styles.itemBlock)} onClick={item.onClick} key={item.key}>
-                                            {item.icon ? (
-                                                <img src={`/img/menu/${item.icon}`} />
-                                            ) : (
-                                                <FontAwesomeIcon
-                                                    icon={item.faIcon!}
-                                                    style={{
-                                                        width: '24px',
-                                                        height: '24px',
-                                                    }}
-                                                />
-                                            )}
-                                            {!isCollapsed && (
-                                                <span className={cx('ms-4', styles.itemLink)}>{item.label}</span>
-                                            )}
-                                        </div>
-                                    ),
-                                )}
+    const handleMouseLeave = () => {
+        if (!isCollapsed) {
+            return;
+        }
+
+        setShowItem(null);
+    };
+
+    return (
+        <aside className={cx(styles.container, { [styles.collapsed]: isCollapsed })}>
+            <div style={{ height: '100%' }} className="d-flex flex-column justify-content-between">
+                <div>
+                    <div className={styles.header}>
+                        {!isCollapsed && <span className={styles.beta}>BETA</span>}
+                        {isCollapsed ? <img src="/img/logoSmall.svg" /> : <img src="/img/logoBig.svg" />}
+                    </div>
+
+                    <div className={cx(styles.bar, { [styles.barLoading]: true })} />
+
+                    <div style={{ marginTop: isCollapsed ? '30px' : 0 }}>
+                        {sections.map((section, index) => (
+                            <div key={index}>
+                                {!isCollapsed && <div className={styles.sectionLabel}>{section.name}</div>}
+
+                                <div>
+                                    {section.items.map((item) =>
+                                        item.url ? (
+                                            <NavLink
+                                                className={cx(styles.itemBlock, {
+                                                    [styles.hovered]: showItem === item.key,
+                                                })}
+                                                activeClassName={styles.selectedItem}
+                                                to={item.url}
+                                                key={item.key}
+                                                onMouseEnter={() => handleMouseEnter(item)}
+                                                onMouseLeave={handleMouseLeave}
+                                            >
+                                                {item.icon ? (
+                                                    <img src={`/img/menu/${item.icon}`} />
+                                                ) : (
+                                                    <FontAwesomeIcon
+                                                        icon={item.faIcon!}
+                                                        style={{
+                                                            width: '24px',
+                                                            height: '24px',
+                                                        }}
+                                                    />
+                                                )}
+
+                                                {(!isCollapsed || showItem === item.key) && (
+                                                    <span
+                                                        className={cx('ms-4', styles.itemLink, {
+                                                            [styles.showCollapsed]: showItem === item.key,
+                                                        })}
+                                                    >
+                                                        {item.label}
+                                                    </span>
+                                                )}
+                                            </NavLink>
+                                        ) : (
+                                            <div className={cx(styles.itemBlock)} onClick={item.onClick} key={item.key}>
+                                                {item.icon ? (
+                                                    <img src={`/img/menu/${item.icon}`} />
+                                                ) : (
+                                                    <FontAwesomeIcon
+                                                        icon={item.faIcon!}
+                                                        style={{
+                                                            width: '24px',
+                                                            height: '24px',
+                                                        }}
+                                                    />
+                                                )}
+                                                {!isCollapsed && (
+                                                    <span className={cx('ms-4', styles.itemLink)}>{item.label}</span>
+                                                )}
+                                            </div>
+                                        ),
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
-            </div>
 
-            {footer && (
-                <div className={styles.footer}>
-                    {footer.map((item) => (
-                        <div className={styles.itemBlock} onClick={item.onClick} key={item.key}>
-                            {item.icon ? (
-                                <img src={`/img/menu/${item.icon}`} />
-                            ) : (
-                                <FontAwesomeIcon icon={item.faIcon!} style={{ width: '24px', height: '24px' }} />
-                            )}
-                            {!isCollapsed && <div className="ms-4">{item.label}</div>}
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
-    </aside>
-);
+                {footer && (
+                    <div className={styles.footer}>
+                        {footer.map((item) => (
+                            <div className={styles.itemBlock} onClick={item.onClick} key={item.key}>
+                                {item.icon ? (
+                                    <img src={`/img/menu/${item.icon}`} />
+                                ) : (
+                                    <FontAwesomeIcon icon={item.faIcon!} style={{ width: '24px', height: '24px' }} />
+                                )}
+                                {!isCollapsed && <div className="ms-4">{item.label}</div>}
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </aside>
+    );
+};
