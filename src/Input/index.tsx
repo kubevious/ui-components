1e7eecb0d2ff94be9d578c789c6d93b743c59009
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-12 11:45:12 -0700

    fixes to input.

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -9,14 +9,18 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     hasError?: boolean;
     rightIcon?: ReactNode;
     containerClassName?: string;
+
+    ref?: any;
 }
 
-export const Input: FC<InputProps> = ({ label, hasError, rightIcon, containerClassName, ...rest }) => (
+export const Input: FC<InputProps> = ({ label, hasError, rightIcon, containerClassName, ref, ...rest }) => (
     <div className={cx(styles.container, containerClassName )}>
         {label && <Label text={label} >
             </Label>}
         <div className={styles.inputContainer}>
-            <input className={cx(styles.input, { [styles.hasError]: hasError })} {...rest} />
+            <input className={cx(styles.input, { [styles.hasError]: hasError })}
+                   ref={ref}
+                   {...rest} />
             {rightIcon && <div className={styles.iconContainer}>
                 {rightIcon}
             </div>}
